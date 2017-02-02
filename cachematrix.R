f08945b8f268ca0500d7cc35f7e71638e49f5444
rajdev <rajdev@nyu.edu>
2017-02-02 09:41:31 -0800

    Updated cachematrix.R file with solution

--- cachematrix.R
+++ cachematrix.R
@@ -7,15 +7,18 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   inverse <- NULL
+  # set matrix data and inverse=NULL in another env
   set <- function(y) { 
     x <<- y 
     inverse <<- NULL
   }
   get <- function() x
+  # setter for Inverse
   setInverse <- function(inv) {
     inverse <<- inv
   }
   getInverse <- function() inverse
+  # Return getter-and setter functions for data, Inverse
   list(set= set, get= get, setInverse= setInverse, getInverse= getInverse)
 }
 
@@ -26,15 +29,18 @@ makeCacheMatrix <- function(x = matrix()) {
 ## the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  # Get the inverse
   inverse <- x$getInverse()
+  # if not null return inverse
   if(!is.null(inverse)) {
     message('Getting cached inverse')
     return(inverse)
   }
+  # code reaches here only if inverse is null, then get data, solve for inverse
   data <- x$get()
   #print(data)
   inverse <- solve(data,...)
+  # Set the inverse and return value
   x$setInverse(inverse)
   inverse
 }
